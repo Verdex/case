30eec249a0f622678b204dd56b4eff059912ae5a
David L. Inman <davidlevere@gmail.com>
2023-03-08 23:12:16 -0500

    todo

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let x = parsing::lexer::lex(&input).unwrap();
+        // TODO: renounce leaves the iterator mostly alone on a fatal error, so you can do input.next to
+        // see what the cause of the fatal result was in addition to looking at the parse error contents
+
+        let mut input = input.char_indices();
+        let x = parsing::lexer::lex(&mut input).unwrap();
         println!("{:?}", x);
     } 
 }
