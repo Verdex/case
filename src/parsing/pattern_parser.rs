9c22e3060df4093be88b0909b9789464dcb269e2
David L. Inman <davidlevere@gmail.com>
2023-03-19 10:47:24 -0400

    tuple pattern parser inc prog

--- src/parsing/pattern_parser.rs
+++ src/parsing/pattern_parser.rs
@@ -24,11 +24,21 @@ use crate::data::*;
 
 type ExprParser<T> = fn(&mut T) -> Result<Expr, ParseError>;
 
-pub fn parse_pattern<'a, T>(input : &mut T, expr_parser : ExprParser<T>) -> Result<Pat, ParseError> 
-    where T : Iterator<Item = (usize, &'a Lexeme)> + Clone {
-
-    alt!( input => parse_unbound_variable )
+macro_rules! p {
+    ($vis:vis $name:ident $input:ident $expr_parser:ident $b:block) => { 
+        $vis fn $name<'a, T>($input : &mut T, $expr_parser : ExprParser<T>) -> Result<Pat, ParseError> 
+            where T : Iterator<Item = (usize, &'a Lexeme)> + Clone {
+            
+            $b
+        }
+    }
 }
+
+p!( pub parse_pattern input expr_parser {
+
+    alt!( input => parse_unbound_variable; parse_float; parse_symbol; |x| parse_tuple(x, expr_parser) )
+});
+
 
 pat!(parse_float<'a> : (usize, &'a Lexeme) => Pat = 
     (i, Lexeme::Float { value, .. }) => Pat::Float { value: *value, l_start: i, l_end: i });
@@ -41,3 +51,63 @@ pat!(parse_r_paren<'a> : (usize, &'a Lexeme) => (usize, &'a Lexeme) = x @ (_, Le
 
 pat!(parse_unbound_variable<'a> : (usize, &'a Lexeme) => Pat = 
     (i, Lexeme::Symbol{ value, .. }) => Pat::UnboundVariable { value: value.to_string(), l_start: i, l_end: i });
+
+p!( parse_tuple input expr_parser {
+    p!( parse_pat_comma input expr_parser {
+        // TODO put start and stop data in return type?
+        parser!(input => {
+            pat <= |x| parse_pattern(x, expr_parser);
+            comma <= parse_comma;
+            select pat 
+        })
+    });
+
+    parser!(input => {
+        l_paren <= parse_l_paren;
+        pats <= * |x| parse_pat_comma(x, expr_parser);
+        maybe_last_pat <= ? |x| parse_pattern(x, expr_parser);
+        r_paren <= parse_r_paren;
+        select {
+            let mut pats = pats;
+            match maybe_last_pat {
+                Some(last_pat) => { pats.push(last_pat); },
+                _ => { },
+            }
+
+            Pat::Tuple { params: pats, l_start: l_paren.0, l_end: r_paren.0 }
+        }
+    })
+});
+
+#[cfg(test)]
+mod test {
+    use intra::*;
+
+    use super::*;
+    use super::super::lexer::lex;
+
+    fn slice<'a, T>(input : &'a Vec<T>) -> &'a [T] { &input[..] }
+
+    fn stub_parse_expr<'a>(input : &mut (impl Iterator<Item = (usize, &'a Lexeme)> + Clone)) -> Result<Expr, ParseError> {
+        Ok(Expr::Symbol{ value: "symbol".to_string(), l_start: 0, l_end: 0 })
+    }
+
+    #[test]
+    fn parse_pattern_should_parse_tuple() {
+        let input = "(1, 2, 3)";
+        let mut input = input.char_indices();
+        let ls = lex(&mut input).unwrap();
+        let mut ls = ls.iter().enumerate();
+        let output = parse_pattern(&mut ls, stub_parse_expr).unwrap();
+
+        let mut matched = false;
+        atom!( output => [Pat::Tuple { ref params, .. }] params
+                       ; slice $ [[ Pat::Float { value: 1.0, .. }
+                                  , Pat::Float { value: 2.0, .. }
+                                  , Pat::Float { value: 3.0, .. } 
+                                 ]]  
+                      => { matched = true; } );
+        assert!(matched);
+    }
+
+}
